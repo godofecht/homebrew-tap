918aef721b53c106f574e6051056950ca4183c56
godofecht <abhishek.shivakumar@gmail.com>
2026-07-29 02:07:53 +0100

    whispergo 0.9.3

--- Casks/whispergo.rb
+++ Casks/whispergo.rb
@@ -1,6 +1,6 @@
 cask "whispergo" do
-  version "0.9.2"
-  sha256 "0a8d9d3fb15436992de053a002d63b7037d7f2c341bb178972d228d83177b8de"
+  version "0.9.3"
+  sha256 "bf68d32281dbf3e3d77419467954cee8aeb1ca8e50d276b73440a8ea984aab89"
 
   url "https://quilio.dev/downloads/WhisperGo-#{version}-macOS-arm64.dmg"
   name "WhisperGo"
